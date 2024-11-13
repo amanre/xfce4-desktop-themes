e8bc24f0cd1687a2ce610a8c336eb3eee35ff7bb
amanre <amanre556@gmail.com>
2024-11-13 11:34:26 +0100

    update

--- abstract-xfce-theme/files/xfce-themes/Abstract.bash
+++ abstract-xfce-theme/files/xfce-themes/Abstract.bash
@@ -35,8 +35,8 @@ change_dock() {
 }
 
 # Geany
-#geany_colors=catppucin.conf'
-#geany_font='JetBrains Mono 10'
+geany_colors='kurayami.conf'
+geany_font='JetBrains Mono 11'
 
 # Appearance
 xfwm_theme='Gruvbox-Dark-BL'
