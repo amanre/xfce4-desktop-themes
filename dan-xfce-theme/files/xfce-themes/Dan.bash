3aed45315c1c74baba515c6a4c75926bf42c7610
amanre <amanre556@gmail.com>
2024-11-17 11:54:05 +0100

    update

---
+++ dan-xfce-theme/files/xfce-themes/Dan.bash
@@ -0,0 +1,46 @@
+# Wallpaper
+wallpaper='/usr/share/backgrounds/walls/arco.jpg'
+
+# Terminal
+wal -i /usr/share/backgrounds/walls/arco.jpg
+/usr/share/amos/pywal-conky/refresh_conky.sh
+terminal_font_name='Source Code Pro '
+terminal_font_size='13'
+
+# Plank -------------------------------------
+change_dock() {
+	cat > "$HOME"/.cache/plank.conf <<- _EOF_
+		[dock1]
+		alignment='center'
+		auto-pinning=true
+		current-workspace-only=false
+		dock-items=['xfce-settings-manager.dockitem', 'Alacritty.dockitem', 'thunar.dockitem', 'firefox.dockitem', 'geany.dockitem']
+		hide-delay=0
+		hide-mode='intelligent'
+		icon-size=42
+		items-alignment='center'
+		lock-items=true
+		monitor=''
+		offset=0
+		pinned-only=false
+		position='bottom'
+		pressure-reveal=false
+		show-dock-item=false
+		theme='Tokyonight-Storm-BL'
+		tooltips-enabled=true
+		unhide-delay=0
+		zoom-enabled=true
+		zoom-percent=160
+	_EOF_
+}
+
+# Geany
+geany_colors='dracula.conf'
+geany_font='JetBrains Mono 11'
+
+# Appearance
+xfwm_theme='Dracula-standard-buttons'
+gtk_font='Noto Sans 13'
+gtk_theme='Dracula-standard-buttons'
+icon_theme='Tela-circle-dracula-dark'
+cursor_theme='Dracula-cursors'
